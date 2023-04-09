26d46705db4540bdccd5ed0401e5eb7909ea7de6
Eric Earl <eric.earl@nih.gov>
2023-04-09 15:01:12 -0700

    Update main.js
    Eff the Google Sheets, roll with object literal!

--- scripts/main.js
+++ scripts/main.js
@@ -1,41 +1,40 @@
-
 $(function() {
 
     // Populate using an object literal
 
-    // WTF.init({
+    WTF.init({
 
-    //     heading: [
-    //         "It's a fucking",
-    //         "Check this shit out, a fucking"
-    //     ],
-    //     response: [
-    //         "Already fucking seen one",
-    //         "Give me a-fucking-nother one",
-    //         "Don't really give a fuck"
-    //     ],
-    //     template: [
-    //         "Big @color @animal",
-    //         "Silly @animal with @color fur"
-    //     ],
-    //     animal: [
-    //         "dog",
-    //         "cat",
-    //         "rabbit"
-    //     ],
-    //     color: [
-    //         "red",
-    //         "blue",
-    //         "green",
-    //         "yellow"
-    //     ]
-    // });
+        heading: [
+            "It's a fucking",
+            "Check this shit out, a fucking"
+        ],
+        response: [
+            "Already fucking seen one",
+            "Give me a-fucking-nother one",
+            "Don't really give a fuck"
+        ],
+        template: [
+            "Big @color @animal",
+            "Silly @animal with @color fur"
+        ],
+        animal: [
+            "dog",
+            "cat",
+            "rabbit"
+        ],
+        color: [
+            "red",
+            "blue",
+            "green",
+            "yellow"
+        ]
+    });
 
-    // Populate using a JSON file
-    // WTF.init( 'sample.json' );
+    // // Populate using a JSON file
+    // // WTF.init( 'sample.json' );
 
-    // Populate using a Google spreadsheet ID (you must publish it first!)
-    // @see https://support.google.com/drive/answer/37579?hl=en
-    WTF.init( '2PACX-1vR8CGgeY30igIX29CrsV1E6m8vbwybFLtD7hmMuQgtNi2Jrz3Ce48flXl3QO-xbQDZpyOhIQ-JpmiQ1' );
+    // // Populate using a Google spreadsheet ID (you must publish it first!)
+    // // @see https://support.google.com/drive/answer/37579?hl=en
+    // WTF.init( '2PACX-1vR8CGgeY30igIX29CrsV1E6m8vbwybFLtD7hmMuQgtNi2Jrz3Ce48flXl3QO-xbQDZpyOhIQ-JpmiQ1' );
 
 });
